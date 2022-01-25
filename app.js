b20923777b6e1e587f885e6f1c8858a7f7de98f0
liuxinqi <liuxinqi@gridsum.com>
2022-01-25 16:58:02 +0800

    feat: 处理请求体

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
+'use strict'
+
 const {URL}=require("url")
 
 const {ErrorModel}=require("./src/model/responseModel")
 const listRoute=require("./src/router/blogRoute")
 
-const handler=(req,res)=>{
+const getRequestBody= (req)=>{
+    return new Promise((resolve,reject)=>{
+        let data=''
+        req.on("data",chunk=>{
+            data+=chunk.toString()
+        })
+        req.on("error",err=>{
+            reject(err)
+        })
+        req.on("end",()=>{
+            if(!data){
+                resolve({}) 
+            }
+             resolve(data)
+        })
+    })
+   
+}
+
+const handler=async (req,res)=>{
     res.setHeader("Content-Type",'application/json')
 
     const {searchParams,pathname} = new URL(req.url,"http://"+req.headers.host);
     console.log(req.method,pathname);
+
     req.query=Object.fromEntries(searchParams)
 
+    req.body =  await getRequestBody(req)
+
+    
     const list= listRoute(req,res,pathname)
-    res.end(JSON.stringify(list))
+    if(list){
+       return res.end(JSON.stringify(list))
+    }
+  
+    res.end()
 
 }
 
